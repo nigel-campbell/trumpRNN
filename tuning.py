bac2cf86b1574dd069addd3998bd81bf4eea56a0
Bryan Baek <seungkwan.baek@gmail.com>
2019-03-31 11:22:15 -0400

    included outputs from my models

--- tuning.py
+++ tuning.py
@@ -6,16 +6,21 @@ learning_rate = [0.001, 0.01]
 chunk_len = [100, 150]
 batch_size = [150, 200]
 
+count = 0
 for epoch in n_epochs:
 	for hidden in hidden_size:
 		for lr in learning_rate:
 			for chunk in chunk_len:
 				for batch in batch_size:
-					subprocess.call(['python', '../src/char_rnn/train.py', 
-						'../data/fulldata.txt', 
-						'--n_epochs', str(epoch),
-						'--hidden_size', str(hidden), 
-						'--learning_rate', str(lr), 
-						'--chunk_len', str(chunk), 
-						'--batch_size', str(batch), 
-						'--cuda'])
+					if count < 2:
+						count += 1
+						continue
+					else:
+						subprocess.call(['python', '../src/char_rnn/train.py', 
+							'../data/fulldata.txt', 
+							'--n_epochs', str(epoch),
+							'--hidden_size', str(hidden), 
+							'--learning_rate', str(lr), 
+							'--chunk_len', str(chunk), 
+							'--batch_size', str(batch), 
+							'--cuda'])
