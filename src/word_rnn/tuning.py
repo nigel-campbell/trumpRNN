d00c807cd6474c35c9afdc6e4b8ccafc1f6c7bc0
Nigel Campbell <ncampbell7@gatech.edu>
2019-03-31 19:01:02 -0400

    Updated tuning script

--- src/word_rnn/tuning.py
+++ src/word_rnn/tuning.py
@@ -9,6 +9,7 @@ n_layers = [2, 5, 10]
 epochs = [6, 40, 100]
 dropout = [0.0, 0.2]
 
+model_dir = 'models'
 count = 0
 for model in models:
     for em_size in em_sizes:
@@ -16,12 +17,13 @@ for model in models:
             for layer_count in n_layers:
                 for epoch_count in epochs:
                     for dropout_count in dropout:
-                        output_file = '{}_{}_{}.pt'.format(model, \
-                                em_size, n_hidden, layer_count, \
+                        output_file = '{}_{}_{}_{}_{}_{}.pt'.format(model, \
+                                em_size, nhidden, layer_count, \
                                 epoch_count, dropout_count)
+                        output_file = '{}/{}'.format(model_dir, output_file)
                         subprocess.call(['python', 'main.py',
                                          '--data', 'data/trump/',
-                                         '--models', model,
+                                         '--model', model,
                                          '--emsize', str(em_size),
                                          '--nhid', str(nhidden),
                                          '--nlayers', str(layer_count),
