a766db90a4cec3eb44450adaefcd687b40b9c139
Nigel Campbell <ncampbell7@gatech.edu>
2019-04-03 17:47:20 -0400

    correcting output path for word_level sample gen.

--- src/word_rnn/generate_word_level.py
+++ src/word_rnn/generate_word_level.py
@@ -5,13 +5,14 @@ from os.path import isfile, join
 
 
 path = 'models/'
+output_path = 'results/'
 files = [f for f in listdir(path) if isfile(join(path, f))]
 print("Generating samples for {} models".format(len(files)))
 for fname in files:
-    outf = fname[:len(files)-2]
-    print("Generating output for {}. Saving to {}".format(fname, outf))
+    output =  output_path + fname.replace('.pt', 'txt')
+    print("Generating output for {}. Saving to {}".format(fname, output))
     subprocess.call(['python', 'generate.py', 
                      '--data', 'data/trump',
-                     '--outf', outf,
+                     '--outf', output,
                      '--checkpoint', path + fname,
                      '--cuda'])
