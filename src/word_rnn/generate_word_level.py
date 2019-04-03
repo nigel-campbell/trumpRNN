0e3d33da372bf1775cb97155290bc2adea8cce45
Nigel Campbell <ncampbell7@gatech.edu>
2019-04-03 17:39:36 -0400

    Added script to generate word level samples.

---
+++ src/word_rnn/generate_word_level.py
@@ -0,0 +1,17 @@
+import subprocess
+
+from os import listdir
+from os.path import isfile, join
+
+
+path = 'models/'
+files = [f for f in listdir(path) if isfile(join(path, f))]
+print(files)
+for fname in files:
+    outf = fname[:len(files)-2]
+    print("Generating output for {}. Saving to {}".format(fname, outf))
+    subprocess.call(['python', 'generate.py', 
+                     '--data', 'data/trump',
+                     '--outf', outf,
+                     '--checkpoint', path + fname,
+                     '--cuda'])
