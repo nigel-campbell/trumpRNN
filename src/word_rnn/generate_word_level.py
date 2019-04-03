58eb912055a90060c63163be814342d17109766a
Nigel Campbell <ncampbell7@gatech.edu>
2019-04-03 17:49:57 -0400

    Minor fix.

--- src/word_rnn/generate_word_level.py
+++ src/word_rnn/generate_word_level.py
@@ -9,7 +9,7 @@ output_path = 'results/'
 files = [f for f in listdir(path) if isfile(join(path, f))]
 print("Generating samples for {} models".format(len(files)))
 for fname in files:
-    output =  output_path + fname.replace('.pt', 'txt')
+    output =  output_path + fname.replace('.pt', '.txt')
     print("Generating output for {}. Saving to {}".format(fname, output))
     subprocess.call(['python', 'generate.py', 
                      '--data', 'data/trump',
