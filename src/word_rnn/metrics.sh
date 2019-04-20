e34401c31606cf123c11dd84aa8dfa78057b3f44
Nigel Campbell <ncampbell7@gatech.edu>
2019-04-20 18:53:56 -0400

    Updated output location of metrics.

--- src/word_rnn/metrics.sh
+++ src/word_rnn/metrics.sh
@@ -1,4 +1,8 @@
-python main.py --data data/trump --model GRU --emsize 200 --nhid 500 --nlayers 2 --dropout 0.2 --epochs 100 --cuda
-python main.py --data data/trump --model GRU --emsize 200 --nhid 500 --nlayers 2 --dropout 0.2 --epochs 40 --cuda
-python main.py --data data/trump --model GRU --emsize 200 --nhid 500 --nlayers 2 --dropout 0.2 --epochs 6 --cuda
+python main.py --data data/trump --model GRU --emsize 200 --nhid 500 --nlayers 2 --dropout 0.2 --epochs 100 --cuda --save "100.dat"
+
+
+python main.py --data data/trump --model GRU --emsize 200 --nhid 500 --nlayers 2 --dropout 0.2 --epochs 40 --cuda --save "40.dat"
+
+
+python main.py --data data/trump --model GRU --emsize 200 --nhid 500 --nlayers 2 --dropout 0.2 --epochs 6 --cuda --save "6.dat"
 
