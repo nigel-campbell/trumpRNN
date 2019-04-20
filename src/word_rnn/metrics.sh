5309d0bce84ee0d0b3465427658ca1e1792d3282
Nigel Campbell <ncampbell7@gatech.edu>
2019-04-20 18:50:11 -0400

    Added script for generating metrics.

---
+++ src/word_rnn/metrics.sh
@@ -0,0 +1,4 @@
+python main.py --data data/trump --model GRU --emsize 200 --nhid 500 --nlayers 2 --dropout 0.2 --epochs 100 --cuda
+python main.py --data data/trump --model GRU --emsize 200 --nhid 500 --nlayers 2 --dropout 0.2 --epochs 40 --cuda
+python main.py --data data/trump --model GRU --emsize 200 --nhid 500 --nlayers 2 --dropout 0.2 --epochs 6 --cuda
+
